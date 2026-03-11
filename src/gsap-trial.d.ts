3ed0c445da77b9f6b6567fe6b9b16d9c3f5f4f28
Urvil004 <urvildarji3110@gmail.com>
2026-03-11 19:55:28 +0530

    fix: replace gsap-trial with free gsap package

--- src/gsap-trial.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'gsap-trial/SplitText';
-declare module 'gsap-trial/ScrollTrigger';
-declare module 'gsap-trial/ScrollToPlugin';
-declare module 'gsap-trial/DrawSVGPlugin';
-declare module 'gsap-trial/MorphSVGPlugin';
-declare module 'gsap-trial/*';
